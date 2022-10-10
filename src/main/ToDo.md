df73dc833e729683abe7f4b55b67c81721e39a60
UlfHéden <uffeheden@gmail.com>
2022-10-10 14:52:31 +0200

    Controllers Item / User

---
+++ src/main/ToDo.md
@@ -0,0 +1,18 @@
+**Must have**
+-Database
+-Login
+-Ads
+
+
+
+**Nice to have**
+Javasscript registrated user / ads popup
+
+
+
+**Who´s doing what today?**
+Derya:
+Tobias:
+Mohamed:
+Haidar:
+Ulf:
--- src/main/java/ItemController.java
+++ src/main/java/ItemController.java
@@ -1,2 +1,24 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.sql.DataSource;
+
+
+@Controller
 public class ItemController {
+
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping("/")
+    String start() {
+        logger.info("Logging is running");
+        return "startpage";
+    }
+
 }
