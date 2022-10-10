9317db12aab763481d8b7bba81c64f10622c7b2e
UlfHéden <uffeheden@gmail.com>
2022-10-10 14:19:39 +0200

    Ground Structure update

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,2 +1,82 @@
-public class Item {
-}
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+    @Entity
+    public class Item {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int id;
+        private String name;
+        private String category;
+        private String location;
+        private int qty;
+        private String deliveryType;
+
+        public Item() {
+        }
+
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCategory() {
+            return category;
+        }
+
+        public void setCategory(String category) {
+            this.category = category;
+        }
+
+        public String getLocation() {
+            return location;
+        }
+
+        public void setLocation(String location) {
+            this.location = location;
+        }
+
+        public int getQty() {
+            return qty;
+        }
+
+        public void setQty(int qty) {
+            this.qty = qty;
+        }
+
+        public String getDeliveryType() {
+            return deliveryType;
+        }
+
+        public void setDeliveryType(String deliveryType) {
+            this.deliveryType = deliveryType;
+        }
+
+        public Item(int id, String name, String category, String location, int qty, String deliveryType) {
+            this.id = id;
+            this.name = name;
+            this.category = category;
+            this.location = location;
+            this.qty = qty;
+            this.deliveryType = deliveryType;
+
+
+
+        }
+    }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,84 @@
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int orgnr;
+    private String city;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getOrgnr() {
+        return orgnr;
+    }
+
+    public void setOrgnr(int orgnr) {
+        this.orgnr = orgnr;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(int id, String name, int orgnr, String city, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.orgnr = orgnr;
+        this.city = city;
+        this.email = email;
+        this.password = password;
+
+
+    }
+
 }
+
