3943bd70a469322cc3cf75f85613dae02d76ec6b
HaidarE <98402448+HaidarE@users.noreply.github.com>
2022-10-11 10:15:30 +0200

    H2 databas

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -1,3 +1,5 @@
+package com.example.demo;
+
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import javax.persistence.Entity;
--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,3 +1,5 @@
+package com.example.demo;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
