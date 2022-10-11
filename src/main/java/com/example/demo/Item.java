1a99880139dfbc2d6ebd1e9e3956ab180765cd35
HaidarE <98402448+HaidarE@users.noreply.github.com>
2022-10-11 11:33:11 +0200

    Getmapping

--- src/main/java/com/example/demo/Item.java
+++ src/main/java/com/example/demo/Item.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 
@@ -10,7 +8,6 @@ import javax.persistence.Id;
     public class Item {
 
         @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
         private int id;
         private String name;
         private String category;
--- src/main/java/com/example/demo/ItemController.java
+++ src/main/java/com/example/demo/ItemController.java
@@ -18,9 +18,24 @@ public class ItemController {
     private DataSource dataSource;
     @GetMapping("/")
     String start() {
-        logger.info("Logging is running");
+        logger.info("start is running");
         return "startpage";
     }
 
+    @GetMapping("/iteam")
+    String itempage(){
+        logger.info("itempage is running");
+        return "iteamPage";
+    }
+    @GetMapping("/iteamCreat")
+    String iteamCreat(){
+        logger.info("iteamCreat is running");
+        return "createIteam";
+    }
+    @GetMapping("/iteamDetails")
+    String iteamDetails(){
+        logger.info("iteamDetails is running");
+        return "itemDetails";
+    }
 
 }
