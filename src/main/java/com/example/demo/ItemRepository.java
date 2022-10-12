037e80ee8559a2d484641db4c69f67a162428ebf
DeryaBaykal <derya.baykal91@gmail.com>
2022-10-12 09:45:47 +0200

    Added @Service to itemRepository

--- src/main/java/com/example/demo/ItemRepository.java
+++ src/main/java/com/example/demo/ItemRepository.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface ItemRepository extends CrudRepository<Item,Integer> {
+
 }
