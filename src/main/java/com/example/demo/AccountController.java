3baf5a384138176d8f23d88ed123be61d9e59506
UlfHéden <uffeheden@gmail.com>
2022-10-13 13:51:25 +0200

    logout mapping / end session and logoutfunction in admin page

--- src/main/java/com/example/demo/AccountController.java
+++ src/main/java/com/example/demo/AccountController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
 
@@ -30,6 +32,13 @@ import javax.sql.DataSource;
         String admin(){
             logger.info("admin is running");
             return "admin";
+        }
+
+        @GetMapping("/logoutuser")
+        public String logout(HttpSession session, HttpServletResponse res)
+        {
+            session.removeAttribute("siteuser");
+            return "redirect:/";
         }
 
 
